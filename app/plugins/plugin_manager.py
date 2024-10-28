ca62a6f826de7f463341b6fbde0daa4e644fb649
Jkowal012 <jk262@njit.edu>
2024-10-28 17:46:53 -0400

    fixed code for plugin manager not working

--- app/plugins/plugin_manager.py
+++ app/plugins/plugin_manager.py
@@ -9,12 +9,16 @@ class PluginManager:
 
     def load_plugins(self):
         plugins_dir = os.path.dirname(__file__)
-        for filename in os.listdir(plugins_dir):
-            if filename.endswith('.py') and filename not in ['__init__.py', 'plugin_manager.py']:
-                module_name = filename[:-3]
-                module = importlib.import_module(f"apps.plugins.{module_name}")
-                if hasattr(module, 'command_name') and hasattr(module, 'run'):
-                    self.plugins[module.command_name] = module.run
+        for foldername in os.listdir(plugins_dir):
+            folder_path = os.path.join(plugins_dir, foldername)
+            if os.path.isdir(folder_path) and foldername not in ['__pycache__', 'plugin_manager']:
+                module_name = f"apps.plugins.{foldername}"
+                try:
+                    module = importlib.import_module(module_name)
+                    if hasattr(module, 'command_name') and hasattr(module, 'run'):
+                        self.plugins[module.command_name] = module.run
+                except ModuleNotFoundError:
+                    print(f"Could not load plugin: {module_name}")
 
     def execute(self, command_name, args):
         if command_name in self.plugins:
