e737d8238b393855aa14c147d72e6b783d9d1ab3
Jkowal012 <jk262@njit.edu>
2024-10-28 18:31:24 -0400

    plugins fixes

--- app/plugins/plugin_manager.py
+++ app/plugins/plugin_manager.py
@@ -12,7 +12,7 @@ class PluginManager:
         for foldername in os.listdir(plugins_dir):
             folder_path = os.path.join(plugins_dir, foldername)
             if os.path.isdir(folder_path) and foldername not in ['__pycache__', 'plugin_manager']:
-                module_name = f"apps.plugins.{foldername}"
+                module_name = f"app.plugins.{foldername}"
                 try:
                     module = importlib.import_module(module_name)
                     if hasattr(module, 'command_name') and hasattr(module, 'run'):
